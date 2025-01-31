93f5d9b3a511178d300ac8959746ebf4bdb78993
Lad198 <155990475+Lad198@users.noreply.github.com>
2025-01-31 00:00:14 -0500

    forgot to commit before submitting

--- 1774 Project 1/Solution.py
+++ 1774 Project 1/Solution.py
@@ -17,12 +17,13 @@ class Solution:
         self.circuit.get_g_elements()
         # Building the matrix
         self.cond_matrix[0,0] = self.circuit.get_g_elements()
-        self.cond_matrix[0, 1] = self.circuit.get_g_elements()
-        self.cond_matrix[1, 0] = self.circuit.get_g_elements()
+        self.cond_matrix[0, 1] = -self.circuit.get_g_elements()
+        self.cond_matrix[1, 0] = -self.circuit.get_g_elements()
         self.cond_matrix[1, 1] = self.circuit.get_g_elements() + self.circuit.loads["Lb"].g
+
     def do_power_flow(self):
         self.build_conductance_matrix()
         R_matrix = np.linalg.inv(self.cond_matrix)
         self.circuit.set_i(self.circuit.vsource.v/R_matrix[0,0])
         self.circuit.voltages[self.circuit.resistors["Rab"].bus1] = self.circuit.vsource.v
-        self.circuit.voltages[self.circuit.resistors["Rab"].bus2] = self.circuit.vsource.v - self.circuit.i * R_matrix[1,1]
+        self.circuit.voltages[self.circuit.resistors["Rab"].bus2] = (self.circuit.i * R_matrix[1,1])
--- 1774 Project 1/main.py
+++ 1774 Project 1/main.py
@@ -9,9 +9,9 @@ Obj.add_bus("A")
 Obj.add_bus("B")
 
 # Now, add elements (voltage source, resistors, and loads)
-Obj.add_vsource_element("V1", "A", 100)
-Obj.add_resistor_element("Rab", "A", "B", 5)
-Obj.add_load_element("Lb", "B", 2000, 100)
+Obj.add_vsource_element("V1", "A", 200)
+Obj.add_resistor_element("Rab", "A", "B", 250)
+Obj.add_load_element("Lb", "B", 80, 200)
 
 # Solve the power flow
 S = Solution(Obj)
